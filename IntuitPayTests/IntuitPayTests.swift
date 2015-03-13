942862b9a786ae5ee213dd293d27407035e7a718
shdatta <shdatta@gmail.com>
2015-03-13 15:49:51 -0700

    Initial Commit

---
+++ IntuitPayTests/IntuitPayTests.swift
@@ -0,0 +1,36 @@
+//
+//  IntuitPayTests.swift
+//  IntuitPayTests
+//
+//  Created by Nanjappa, Srikanta on 3/13/15.
+//  Copyright (c) 2015 Nanjappa, Srikanta. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class IntuitPayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
